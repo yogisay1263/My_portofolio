0e25368542323cfe8d349ac2c912b4d6cc603bc8
YADLA YOGITHA <96356614+yogisay1263@users.noreply.github.com>
2023-09-25 13:46:17 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <nav>
+        <div className="nav-links-container">
+          <ul className="nav-links">
+            <li><a href="#about">About</a></li>
+            <li><a href="#experience">Experience</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div>
+      </nav>
+      <p>Copyright &copy; 2023 yogitha. All Rights Reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
